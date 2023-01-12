a62398eccd3c8e459f13166832c02a400c64865d
Nguyen Hoang <dopnguyen12@gmail.com>
2023-01-12 11:51:56 +0700

    no commit

---
+++ firstFight.h
@@ -0,0 +1,49 @@
+/*
+    * Ho Chi Minh City University of Technology
+    * Faculty of Computer Science and Engineering
+    * Initial code for Assignment 0
+    * Programming Fundamentals Spring 2021
+    * Author: Nguyen Thanh Nhan
+    * Date: 08.02.2021
+*/
+
+//The library here is concretely set, students are not allowed to include any other libraries.
+#ifndef firstFight_h
+#define firstFight_h
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////
+///STUDENT'S ANSWER BEGINS HERE
+///Complete the following functions
+///DO NOT modify any parameters in the functions.
+////////////////////////////////////////////////////////////////////////
+int getReady(int& HP, const int& ID, int& M, const int& E1){
+    //Complete this function to gain point on task 1
+    return -1;
+}
+
+int firstBattle(int& HP1, int& HP2, const int& ID1, const int& ID2, const int& E2){
+    //Complete this function to gain point on task 2
+    return -1;
+}
+
+int secondBattle(int& HP1, int& HP2, const int& ID1, const int& ID2, const int& E3){
+    //Complete this function to gain point on task 3
+    return -1;
+}
+
+int finalBattle(int& HP1, int& HP2, const int& ID1, const int& ID2, const int& E4){
+    //Complete this function to gain point on task 4
+    return -1;
+}
+////////////////////////////////////////////////
+///END OF STUDENT'S ANSWER
+////////////////////////////////////////////////
+#endif /* firstFight_h */
